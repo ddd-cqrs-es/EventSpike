d14a01f1adcc401e906dc5c9317b976f3172c4d9
Anders Storhaug <storhaug.3@gmail.com>
2016-02-28 15:58:05 -0500

    Experimenting

--- EventSpike.Experiments.CompositionRoot/MultitenantCompositionRootExperiments.cs
+++ EventSpike.Experiments.CompositionRoot/MultitenantCompositionRootExperiments.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
 using EventSpike.SqlIntegration;
+using EventStore.ClientAPI;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Paramol;
 using Paramol.SqlClient;
 
-namespace EventSpike.Experiments.CompositionRoot
+namespace EventSpike.CompositionRootExperiments
 {
     [TestClass]
     public class MultitenantCompositionRootExperiments
@@ -23,24 +31,49 @@ namespace EventSpike.Experiments.CompositionRoot
         // > A Composition Root is application-specific; it's what defines a single application.
         // > After having written nice, decoupled code throughout your code base, the Composition Root is where you finally couple everything, from data access to (user) interfaces.
 
+
         internal class ApplicationCompositionRoot
         {
-            private readonly ConcurrentDictionary<string, TenantCompositionRoot> _tenantRoots = new ConcurrentDictionary<string, TenantCompositionRoot>();
+            private readonly ConcurrentDictionary<string, HandlerCompositionRoot> _handlerRoots = new ConcurrentDictionary<string, HandlerCompositionRoot>();
 
             public ApplicationCompositionRoot()
             {
-                
+                var connection = EventStoreConnection
+                    .Create(ConnectionSettings.Create().Build(), new IPEndPoint(IPAddress.Loopback, 1113));
+
+                var ingressSubscription = connection.SubscribeToStreamFrom("ingress", StreamCheckpoint.StreamStart, false, (subscription, resolvedEvent) =>
+                {
+                    var headerJson = Encoding.UTF8.GetString(resolvedEvent.Event.Metadata);
+                    var header = JObject.Parse(headerJson);
+                    var tenantId = header["tenantId"].Value<string>();
+
+                    var messageJson = Encoding.UTF8.GetString(resolvedEvent.Event.Data);
+                    var messageTypeName = header["$type"].Value<string>();
+                    var messageType = Type.GetType(messageTypeName, false, false);
+                    var message = JsonConvert.DeserializeObject(messageJson, messageType);
+
+                    var handlerRoot = _handlerRoots.GetOrAdd(tenantId, _ => new HandlerCompositionRoot(tenantId));
+
+                    var messages = new[] { message };
+
+                    do
+                    {
+                        messages = messages.Select(msg => handlerRoot.Handle(msg).ToArray()).SelectMany(_ => _).ToArray();
+                    } while (messages.Any());
+                });
             }
         }
-        private class TenantCompositionRoot : IDisposable
+
+        private class HandlerCompositionRoot : IDisposable
         {
             private readonly CompositeDisposable _compositeDisposable = new CompositeDisposable();
+            private readonly Dispatcher<object, IEnumerable<object>> _dispatcher;
 
-            public TenantCompositionRoot(string tenantId)
+            public HandlerCompositionRoot(string tenantId)
             {
                 var connectionSettingsFactory = new ConventionTenantSqlConnectionSettingsFactory(tenantId);
 
-                var dispatcher = new Dispatcher<object, IEnumerable>();
+                _dispatcher = new Dispatcher<object, IEnumerable<object>>();
 
                 var connectionSettings = new
                 {
@@ -48,7 +81,17 @@ namespace EventSpike.Experiments.CompositionRoot
                     Projections = connectionSettingsFactory.GetSettings("Projections")
                 };
 
-                dispatcher.Register<ItemPurchased>(Handlers.Handle);
+                _dispatcher.Register<ItemPurchased>(Handlers.Handle);
+
+                _dispatcher.Register<SqlNonQueryCommand>(command =>
+                {
+                    return Enumerable.Empty<object>();
+                });
+            }
+
+            public IEnumerable<object> Handle<TMessage>(TMessage message)
+            {
+                return _dispatcher.Dispatch(message);
             }
 
             public void Dispose()
@@ -69,7 +112,7 @@ namespace EventSpike.Experiments.CompositionRoot
 
         internal class Handlers
         {
-            public static IEnumerable Handle(ItemPurchased purchased)
+            public static IEnumerable<object> Handle(ItemPurchased purchased)
             {
                 yield return TSql.NonQueryStatement(@"insert into ItemsPurchased (StockKeepingUnit) values (@StockKeepingUnit)", new
                 {
